fed41e3cfc4154dafbfc4145c27fa5c4ff122d83
BerezinD <baron_6600@hotmail.com>
2018-08-30 17:26:37 +0300

    New logo Added comments Cleared warnings Toasts are now custom

--- app/src/main/java/com/example/androidacademyonkotlin/MainActivity.kt
+++ app/src/main/java/com/example/androidacademyonkotlin/MainActivity.kt
@@ -8,35 +8,43 @@ import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
+// do some stuff when main display (activity) creates
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+    // set new values (with different syntax)
         var counter = 0
-        val textOnButton: TextView = findViewById(R.id.bigText)
         val bigButton: Button = findViewById(R.id.bigButton)
         val createActivityButton = findViewById<Button>(R.id.openActivity)
         val sendEmailButton = findViewById<Button>(R.id.sendE_mail)
         val messageEditor = findViewById<TextView>(R.id.messageEditor)
+        val textOnButton: TextView = findViewById(R.id.bigText)
         textOnButton.setText(R.string.on_first_button)
-
+    // operate with "create activity" Button - put some data in the new activity
         createActivityButton.setOnClickListener {
+            //this function lie in "SecondActivity" Class
             SecondActivity.openSecondActivity(this, counter, messageEditor.text.toString())
         }
 
+    // operate with Big Button - increase counter on click
+    // and, at the same moment - change text in View above
         bigButton.setOnClickListener{
                 counter++
                 textOnButton.text = "$counter"
-            if (counter==5) SecondActivity.openSecondActivity(this, counter)
+    // then open second display (activity) with if, with data
+            if (counter == 5) SecondActivity.openSecondActivity(this, counter, messageEditor.text.toString())
         }
 
+    // operate with e-mail Button
         sendEmailButton.setOnClickListener{
             val sendEmailIntent = Intent(android.content.Intent.ACTION_SEND)
+    // put some extras to new intent
             sendEmailIntent.type = "*/*"
-            sendEmailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, R.string.email_to)
-            sendEmailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, R.string.academy_on_kotlin)
+            sendEmailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, arrayOf(getString(R.string.email_to)))
+            sendEmailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, getString(R.string.academy_on_kotlin))
             sendEmailIntent.putExtra(android.content.Intent.EXTRA_TEXT, messageEditor.text.toString())
-            startActivity(Intent.createChooser(sendEmailIntent, "Send e-mail"))
+    // create implicit intent - send e-mail
+            startActivity(Intent.createChooser(sendEmailIntent, "SEND"))
         }
     }
 }
--- app/src/main/java/com/example/androidacademyonkotlin/SecondActivity.kt
+++ app/src/main/java/com/example/androidacademyonkotlin/SecondActivity.kt
@@ -4,17 +4,21 @@ import android.app.Activity
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
 class SecondActivity : AppCompatActivity() {
-
+    // create static object - for call it from other activity
     companion object {
         var infoOfUser = ""
-
+        // function for create new activity
         fun openSecondActivity(activity: Activity, counter: Int, textMessage: String = "") {
             val secondActivityIntent = Intent(activity, SecondActivity::class.java)
 
+            // put some data to new activity
             secondActivityIntent.putExtra(infoOfUser, counter)
             secondActivityIntent.putExtra("textMessage", textMessage)
 
@@ -22,16 +26,32 @@ class SecondActivity : AppCompatActivity() {
         }
     }
 
+    // function to show toast with custom settings and layout
+    private fun showCustomToast(textMessage: String){
+        val inflater = layoutInflater
+        val layout: View = inflater.inflate(R.layout.custom_toast, null)
+        val text: TextView = layout.findViewById(R.id.tvTitle)
+            text.text = textMessage
+        val image: ImageView = layout.findViewById(R.id.image)
+            image.setImageResource(R.mipmap.ic_launcher_round)
+        val toast = Toast(this)
+            toast.view = layout
+            toast.setGravity(Gravity.TOP, 50, 100)
+            toast.show()
+    }
+
+    // do some stuff when main display (activity) creates
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
-
+        // set new values to extract data from previous activity
         val counter = intent.getIntExtra(infoOfUser, 0)
         val textToSecondActivity = intent.getStringExtra("textMessage")
         val secondBigTextView = findViewById<TextView>(R.id.second_big_text)
 
-        Toast.makeText(this, textToSecondActivity, Toast.LENGTH_LONG).show()
-        Toast.makeText(this, "${getString(R.string.current_counter_is)} $counter", Toast.LENGTH_LONG).show()
+        // show toast and fill Text field
+        showCustomToast(textToSecondActivity)
+        showCustomToast("${getString(R.string.current_counter_is)} $counter")
         secondBigTextView.text = textToSecondActivity
         secondBigTextView.textSize = 62.5f
     }
